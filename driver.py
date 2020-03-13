e46f1ad88822652938f96cb7c93a4fc3450d24f7
jjtom <jjtom@umich.edu>
2020-03-12 20:14:48 -0400

    Simplified IMU reading acquistion

--- driver.py
+++ driver.py
@@ -14,8 +14,6 @@ ICM20948_I2C_SLV0_REG = 0x04
 ICM20948_I2C_SLV0_CTRL = 0x05
 ICM20948_I2C_SLV0_DO = 0x06
 ICM20948_EXT_SLV_SENS_DATA_00 = 0x3B
-
-
 # Bank 0
 ICM20948_WHO_AM_I = 0x00
 ICM20948_USER_CTRL = 0x03
@@ -60,49 +58,45 @@ def get_decimal(ls, ms):
     high = read_data(ms) << 8
     low = read_data(ls) & 0xff
     return np.int16((high | ls))
-def get_mag_decimal(ms,ls):
-    high = read_mag_data(ms) << 8 # little endian so ls is right 8 bits
-    low = read_mag_data(ls) & 0xff
-    return np.int16((high | low))
+# def get_mag_decimal(ms,ls):
+#     high = read_mag_data(ms) << 8 # little endian so ls is right 8 bits
+#     low = read_mag_data(ls) & 0xff
+#     return np.int16((high | low))
 
 def read_data(num):
     a = bus.read_byte_data(I2C_IMU_ADDRESS, num)
     # print(a)
     return a
 
-def read_mag_data():
+def read_mag_data(ls, ms):
     #mag_write(AK09916_CNTL2, 0x01) #Set magnetometer to single measurement mode
-    ready = mag_read(AK09916_ST1) & 0x01
+    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_USER_CTRL,0b00000000)
     #while (ready != 1): # Wait until data is ready
         #time.sleep(0.00001)
-    set_bank(3) # set bank to 3
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_CTRL, 0x80 | 0x06) # Enable storing data from mgnetometer and 1 byte to read
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_ADDR, AK09916_I2C_ADDR | 0x80) # set slave address and read
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_REG, 0x11) # Read from register
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_DO, 0xff) # Set slave DO?
-    set_bank(0)
-    a = bus.read_i2c_block_data(I2C_IMU_ADDRESS, 0x3b,6) # 3b should be the first address to write to
-    print (a)
-    return a
+    bus.write_byte_data(I2C_IMU_ADDRESS,ICM20948_INT_PIN_CFG , 0b00000010)
+    bus.write_byte_data(0x0c,0x32,0b00010)
+    bus.write_byte_data(0x0c,0x31,0b00010)
+    return get_decimal(ls,ms)
+
 
 
 def set_bank(bank):
     bank = (bank << 4)
     bus.write_byte_data(I2C_IMU_ADDRESS, 0x7f,bank)
-def mag_write(reg,value):
-    set_bank(3) # Set bank to 3
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_ADDR, AK09916_I2C_ADDR) # Set slave 0 address to i2c address of magnetometer
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_REG,reg) # Set register on magnetomer to write to
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_DO,value) # Output data to slave 0
-    set_bank(0) # Switch back to bank 0
-def mag_read(reg):    
-    set_bank(3)
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_CTRL, 0x80 | 1)  # Enable storing data at EXT_SENS_DATA register and number of bytes to 1
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_ADDR, AK09916_I2C_ADDR | 0x80) # set register of magnetomer and set to read
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_REG, reg) #Set register to read from
-    bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_DO, 0xff) # Read data
-    set_bank(0)
-    return read_data(ICM20948_EXT_SLV_SENS_DATA_00)
+# def mag_write(reg,value):
+#     set_bank(3) # Set bank to 3
+#     bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_ADDR, AK09916_I2C_ADDR) # Set slave 0 address to i2c address of magnetometer
+#     bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_REG,reg) # Set register on magnetomer to write to
+#     bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_DO,value) # Output data to slave 0
+#     set_bank(0) # Switch back to bank 0
+# def mag_read(reg):    
+#     set_bank(3)
+#     bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_CTRL, 0x80 | 1)  # Enable storing data at EXT_SENS_DATA register and number of bytes to 1
+#     bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_ADDR, AK09916_I2C_ADDR | 0x80) # set register of magnetomer and set to read
+#     bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_REG, reg) #Set register to read from
+#     bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_I2C_SLV0_DO, 0xff) # Read data
+#     set_bank(0)
+#     return read_data(ICM20948_EXT_SLV_SENS_DATA_00)
 def get_data():
     elapsed_time = t.process_time()
 
@@ -115,10 +109,10 @@ def get_data():
     gyro_z = get_decimal(0x38, 0x37)
     
     # all magnetometer data returns 0 because it is not being read correctly
-    read_mag_data()
-    mag_x =1 #get_mag_decimal(0x3C, 0x3B)
-    mag_y =1 #get_mag_decimal(0x3E, 0x3D)
-    mag_z =1 #get_mag_decimal(0x40, 0x3D)
+    
+    mag_x = read_mag_data(0x11,0x12) #get_mag_decimal(0x3C, 0x3B)
+    mag_y = read_mag_data(0x13,0x14) #get_mag_decimal(0x3E, 0x3D)
+    mag_z = read_mag_data(0x15,0x16) #get_mag_decimal(0x40, 0x3D)
     # print("mag x: ", mag_x,"mag y: ",mag_y,"mag z: ",mag_z)
 
     return np.array([accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, mag_x, mag_y, mag_z])
@@ -130,17 +124,15 @@ def main():
             set_bank(1)
             bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_PWR_MGMT_1, 0x01) # wake up imu from sleep, try until works 
             bus.write_byte_data(I2C_IMU_ADDRESS, ICM20948_PWR_MGMT_2, 0x00) # Set accelerometer and gyroscope to on
-            set_bank(3) # Change to bank 3
-            bus.write_byte_data(I2C_IMU_ADDRESS,ICM20948_I2C_MST_CTRL,0x4d) # Set i2c control for magnetometer. Multimaster capability, and i2c clockspeed of 7, recommended
-            bus.write_byte_data(I2C_IMU_ADDRESS,ICM20948_I2C_MST_DELAY_CTRL,0x01) # Set slave 0 to only be accessed every x samples
-            mag_write(AK09916_CNTL3, 0x01) # soft reset the magnetometer
+            # set_bank(3) # Change to bank 3
+            # bus.write_byte_data(I2C_IMU_ADDRESS,ICM20948_I2C_MST_CTRL,0x4d) # Set i2c control for magnetometer. Multimaster capability, and i2c clockspeed of 7, recommended
+            # bus.write_byte_data(I2C_IMU_ADDRESS,ICM20948_I2C_MST_DELAY_CTRL,0x01) # Set slave 0 to only be accessed every x samples
+            # mag_write(AK09916_CNTL3, 0x01) # soft reset the magnetometer
             success = True
             
         except:
             pass
 
-    while (mag_read(AK09916_CNTL3) == 0x01): # Wait until magnetometer registers are initialized
-        time.sleep(0.0001) #Basically never happens in the first place
     while(True):
         try:
             data = get_data()
